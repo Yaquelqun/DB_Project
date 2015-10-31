16124e4e45c4a71f96cb91df515014b3518ba429
Sandjiv Parassouramanaick <sandjiv.parassou@gmail.com>
2015-10-31 16:00:17 +0100

    jerajoute des trucs !!
    c'est trop biennnn

---
+++ src/Client.java
@@ -0,0 +1,15 @@
+import java.net.* ;
+import javax.swing.*;
+import java.io.* ; 
+
+public class Client extends JFrame  {
+
+	public Client() {
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("coucou toi^^");
+		System.out.println("ça va ?^^");
+	}
+}
